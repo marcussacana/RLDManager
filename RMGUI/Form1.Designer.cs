db8c13070418f06e302f24f0ca5293fef47c67e6
Marcus André <ddtank.marcus@gmail.com>
2017-04-09 17:53:13 -0300

    First Public Build

---
+++ RMGUI/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace RMGUI {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.Strs = new System.Windows.Forms.ListBox();
+            this.Dialog = new System.Windows.Forms.TextBox();
+            this.MS = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MS.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Strs
+            // 
+            this.Strs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Strs.FormattingEnabled = true;
+            this.Strs.ItemHeight = 16;
+            this.Strs.Location = new System.Drawing.Point(12, 12);
+            this.Strs.Name = "Strs";
+            this.Strs.Size = new System.Drawing.Size(595, 388);
+            this.Strs.TabIndex = 0;
+            this.Strs.SelectedIndexChanged += new System.EventHandler(this.Strs_SelectedIndexChanged);
+            // 
+            // Dialog
+            // 
+            this.Dialog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Dialog.Location = new System.Drawing.Point(12, 417);
+            this.Dialog.Name = "Dialog";
+            this.Dialog.Size = new System.Drawing.Size(595, 22);
+            this.Dialog.TabIndex = 1;
+            this.Dialog.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Dialog_KeyPress);
+            // 
+            // MS
+            // 
+            this.MS.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MS.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.MS.Name = "MS";
+            this.MS.Size = new System.Drawing.Size(121, 56);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 451);
+            this.ContextMenuStrip = this.MS;
+            this.Controls.Add(this.Dialog);
+            this.Controls.Add(this.Strs);
+            this.Name = "Form1";
+            this.Text = "RLD Manager GUI";
+            this.MS.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Strs;
+        private System.Windows.Forms.TextBox Dialog;
+        private System.Windows.Forms.ContextMenuStrip MS;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+    }
+}
+
---
+++ RMGUI/Form1.cs
@@ -0,0 +1,53 @@
+﻿using RLDManager;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace RMGUI {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        RLD rld;
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Title = "Select a Script...";
+            fd.Filter = "All ExHIBIT Scripts File|*.rld";
+            if (fd.ShowDialog() == DialogResult.OK) {
+                rld = new RLD(System.IO.File.ReadAllBytes(fd.FileName));
+                string[] STR = rld.Import();
+                Strs.Items.Clear();
+                foreach (string Dialog in STR)
+                    Strs.Items.Add(Dialog);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e) {
+            SaveFileDialog fd = new SaveFileDialog();
+            fd.Title = "Save As...";
+            fd.Filter = "All ExHIBIT Scripts File|*.rld";
+            if (fd.ShowDialog() == DialogResult.OK) {
+                string[] STR = new string[Strs.Items.Count];
+                for (int i = 0; i < STR.Length; i++) {
+                    STR[i] = Strs.Items[i].ToString();
+                }
+                System.IO.File.WriteAllBytes(fd.FileName, rld.Export(STR));
+                MessageBox.Show("File Saved.");
+            }
+        }
+
+        private void Strs_SelectedIndexChanged(object sender, EventArgs e) {
+            if (Strs.SelectedIndex < 0)
+                return;
+            Dialog.Text = Strs.Items[Strs.SelectedIndex].ToString();
+            Text = "ID: " + Strs.SelectedIndex;
+        }
+
+        private void Dialog_KeyPress(object sender, KeyPressEventArgs e) {
+            if (e.KeyChar == '\r' || e.KeyChar == '\n') {
+                Strs.Items[Strs.SelectedIndex] = Dialog.Text;
+                Strs.SelectedIndex++;
+            }
+        }
+    }
+}
